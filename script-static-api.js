38625850ac465353d191600beefea8b3b83316a8
Gonzalo Andrés Alarcón Sarango <gonzandres10@hotmail.com>
2026-07-08 20:59:50 +0200

    Added Javascript to change the clases and ids to text to modify their size depending on the amount of characters. Added authors and random quotes in a function of if else to show also information if the data is not available

--- script-static-api.js
+++ script-static-api.js
@@ -1,11 +1,23 @@
 const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("Twitter");
+const newQuoteBtn = document.getElementById("new-quote");
 
 let apiQuotes = []; 
-
 // show new quote   
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    const quoteText = document.getElementById("quote");
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+    if (quote.text.length > 100) {
+        quoteText.classList.add("long-quote");
+    } else {
+        quoteText.classList.remove("long-quote");
+    }
     quoteText.textContent = quote.text;
 }
 
