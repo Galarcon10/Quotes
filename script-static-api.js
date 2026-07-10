6b5750671fce487cb0abdcc38203912cc90fe0bf
Gonzalo Andrés Alarcón Sarango <gonzandres10@hotmail.com>
2026-07-10 11:34:26 +0200

    Added new links to the nav bar to my github and my linkedin and created a footer with the same style. Also added the footer and the ehader to the Static API Quotes. Corrected the bugs of the nav making classes links ony relevant to the id of the container.

--- script-static-api.js
+++ script-static-api.js
@@ -8,45 +8,57 @@ const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
-// Show new quote   
-function newQuote() {
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    if (!quote.author) {
-        authorText.textContent = "Unknown";
-    } else {
-        authorText.textContent = quote.author;
-    }
-    if (quote.text.length > 100) {
-        quoteText.classList.add("long-quote");
-    } else {
-        quoteText.classList.remove("long-quote");
-    }
-    quoteText.textContent = quote.text;
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
 }
 
-// Get quotes from api  
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// Show new quote
+function newQuote() {
+  loading();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  if (quote.text.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// Get quotes from api
 async function getQuotes() {
-    const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch (error) {
-        console.error("Failed to fetch quotes:", error);
-    }
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    console.error("Failed to fetch quotes:", error);
+  }
 }
 
 // Tweet quote
 function tweetQuote() {
-    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl, "_blank");
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
 }
 
 // Thread quote
 function threadQuote() {
-    const text = `${quoteText.textContent} - ${authorText.textContent}`;
-    const threadUrl = `https://www.threads.net/intent/post?text=${encodeURIComponent(text)}`;
-    window.open(threadUrl, "_blank");
+  const text = `${quoteText.textContent} - ${authorText.textContent}`;
+  const threadUrl = `https://www.threads.net/intent/post?text=${encodeURIComponent(text)}`;
+  window.open(threadUrl, "_blank");
 }
 
 newQuoteBtn.addEventListener("click", newQuote);
@@ -55,7 +67,3 @@ threadBtn.addEventListener("click", threadQuote);
 
 // On load
 getQuotes();
-
-
-
-
